14d98e9c31bc50255defe82e9761aefaeb6e7936
HeineKayn <thomashoger@hotmail.fr>
2023-02-10 15:08:19 +0100

    download dataset

--- projetLib/data.py
+++ projetLib/data.py
@@ -5,7 +5,47 @@ import torch
 
 import random
 
+import requests 
+import shutil
+import py7zr
+import os
+import subprocess
+
 # device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+def download_data(url,dest):
+    zipfile = dest + "VirusTemp.7z"
+    password = "infected"
+     
+    with requests.get(url, stream=True) as r:
+        r.raise_for_status()
+        with open(zipfile, 'wb') as f:
+            for i,chunk in enumerate(r.iter_content(chunk_size=8192)):  
+                f.write(chunk)
+                
+    with py7zr.SevenZipFile(zipfile, mode='r', password=password) as z: 
+        z.extractall() 
+        
+    extracted = url.split("/")[-1]
+    extracted = extracted.split(".")[:-1]
+    unzipped = dest + extracted + "/"
+    
+    entries = os.listdir(dest)
+    for entry in entries:
+        filename = unzipped + entry
+        fileType = subprocess.check_output(filename, shell=True).decode()
+    
+        folder = "other"
+        if "PE" in fileType : folder = "pe"
+        elif "ELF" in fileType : folder = "elf"
+        elif "MS-DOS" in fileType : folder = "msdos"
+
+        hashed = hash(extracted)
+        img_path = f"{dest}{folder}/{hashed}.png"
+        # convert to img
+        
+    os.remove(zipfile)
+    shutil.rmtree(unzipped)
 
 def downloadDataset():
 
@@ -14,8 +54,7 @@ def downloadDataset():
         filenumber = str(filenumber)
         filenumber = "0"*(4-len(filenumber)) + filenumber
         url = "https://samples.vx-underground.org/samples/Blocks/Virusshare%20Collection/Virusshare.{filenumber}.7z"
-
-    # Virusshare.00457.7z
+        download_data(url,"../data/images")
 
 # def getDataset(file:str,factorResize=1,doCrop=True):
 #     process = transforms.Compose([
