6e723b01172935857548afd274c8a8a78a2cce80
HeineKayn <thomashoger@hotmail.fr>
2023-02-20 15:21:35 +0100

    process

--- projetLib/process.py
+++ projetLib/process.py
@@ -3,12 +3,12 @@ from tqdm import tqdm
 import sys
 from statistics import mean
 from torch import nn
+from torchmetrics import ConfusionMatrix
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-def train_malware(net, optimizer, loader, losses, runName="default", epochs=5, lrDecrease=True):
+def train_malware(net, optimizer, loader, losses, testloader=[], runName="default", epochs=5, lrDecrease=True):
     net = net.to(device) 
-    net.train()
     accum_iter = 100 
     lrs = []
     current_lr = optimizer.param_groups[0]["lr"]
@@ -19,17 +19,14 @@ def train_malware(net, optimizer, loader, losses, runName="default", epochs=5, l
     for epoch in t1:
         running_loss = []
         t2 = tqdm(loader, leave=False, colour="#005500", file=sys.stdout) 
+        net.train()
         for batch_idx,(x,y) in enumerate(t2):
             x = x.to(device)
             y = y.to(device).float()
             with torch.set_grad_enabled(True):
                 outputs = net(x)
-
                 try :
-                    outputs = torch.reshape(outputs,(32,))
-                    outputs = (outputs-torch.mean(outputs))/torch.std(outputs)
-                    # predicted = torch.argmax(outputs, dim=1)
-                    # print(predicted)
+                    outputs  = torch.reshape(outputs,(32,))
                     loss = 1e-5
                     for criterion,coef in losses : 
                         loss += criterion(outputs, y)*coef
@@ -39,13 +36,12 @@ def train_malware(net, optimizer, loader, losses, runName="default", epochs=5, l
                     if ((batch_idx + 1) % accum_iter == 0) or (batch_idx + 1 == len(t2)):
                         optimizer.step()
                         optimizer.zero_grad()
-                    t2.set_description(f'Epoch {epoch}, training loss: {mean(running_loss)}, LR : {current_lr}, epoch {epoch + 1}/{epochs}')
+                    t2.set_description(f'Training loss: {mean(running_loss)}, LR : {current_lr}, epoch {epoch + 1}/{epochs}')
                 except Exception as e:
                     pass
-                    # print(e)
-                    # print(outputs.shape)
-        t1.set_description(f'Epoch {epoch + 1}/{epochs}, LR : {current_lr}')
 
+        accuracy = test_malware(net, testloader)
+        t1.set_description(f'Epoch {epoch + 1}/{epochs}, Accuracy {accuracy}, LR : {current_lr}')
         if lrDecrease :        
             current_lr = optimizer.param_groups[0]["lr"]
             lrs.append(current_lr)
@@ -55,11 +51,18 @@ def train_malware(net, optimizer, loader, losses, runName="default", epochs=5, l
 
 def test_malware(net, testloader):
     with torch.no_grad():
-        positive = 0.0
-        for x,y in testloader : 
+        net.eval()
+        matrix = torch.zeros((2,2))
+        confmat = ConfusionMatrix(task="binary", num_classes=2)
+        for x, y in testloader : 
             outputs = net(x)
+            outputs = torch.reshape(outputs,(len(y),))
             m = nn.Sigmoid()
             outputs = m(outputs)
-            positive += torch.sum(outputs == y)
-        positive /= len(testloader.dataset)
-        print(positive*100)
+            matrix  += confmat(outputs, y)
+        (tp,fp),(fn,tn) = matrix
+        accuracy = (tp+tn)/(tp+tn+fp+fn) 
+        return accuracy
+
+        #recall = tp/(tp+fn) # quelle proporition positif a été id correctement 
+        #precision = tp/(tp+fp) # quelle proportion positif = correct
