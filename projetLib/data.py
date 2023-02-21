380f6d7c69afe01c10fe36bb8f742bd8988d8cd1
HeineKayn <thomashoger@hotmail.fr>
2023-02-21 13:35:49 +0100

    Update data.py

--- projetLib/data.py
+++ projetLib/data.py
@@ -23,7 +23,7 @@ def getImageLoader(file:str,resize):
             transforms.Grayscale(),
             transforms.Resize(resize), 
             transforms.ToTensor(),
-            transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+            transforms.Normalize(mean=[0.5], std=[0.5])
             #  transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
     ])
     return ImageFolder(file, process)
