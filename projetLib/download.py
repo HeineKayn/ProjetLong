cc6600b74587b791c9796258f1509ebdc556f7f7
Viktor Mouton <7734587-TotttttoR@users.noreply.gitlab.com>
2023-02-23 10:49:30 +0100

    Snapshot nul

--- projetLib/download.py
+++ projetLib/download.py
@@ -16,49 +16,51 @@ def download_data(url,dest):
     zipname = "./VirusTemp.7z"
     password = "infected"
 
-    with requests.get(url, stream=True) as r:
-        r.raise_for_status()
-        file_size = int(r.headers['Content-Length'])
-        chunk_size = 8192
-        num_bars = int(file_size / chunk_size)
-        t1 = tqdm(r.iter_content(chunk_size=chunk_size), total=num_bars, unit='KB', desc=f"Downloading zip", leave=True, file=sys.stdout)
-        with open(zipname, 'wb') as f:
-            for chunk in t1:  
-                f.write(chunk)
+    # with requests.get(url, stream=True) as r:
+    #     r.raise_for_status()
+    #     file_size = int(r.headers['Content-Length'])
+    #     chunk_size = 8192
+    #     num_bars = int(file_size / chunk_size)
+    #     t1 = tqdm(r.iter_content(chunk_size=chunk_size), total=num_bars, unit='KB', desc=f"Downloading zip", leave=True, file=sys.stdout)
+    #     with open(zipname, 'wb') as f:
+    #         for chunk in t1:  
+    #             f.write(chunk)
                 
-    print("extracting ",zipname)
-    with py7zr.SevenZipFile(zipname, mode='r', password=password) as z: 
-        z.extractall() 
+    # print("extracting ",zipname)
+    # with py7zr.SevenZipFile(zipname, mode='r', password=password) as z: 
+    #     z.extractall() 
         
     extracted = url.split("/")[-1]
     extracted = ".".join(extracted.split(".")[:-1])
     unzipped  = f"./{extracted}/"
-    os.remove(zipname)
+    #os.remove(zipname)
     
     entries = os.listdir(unzipped)
     t1 = tqdm(entries, total=len(entries), desc=f"Extracting features", leave=True, file=sys.stdout)
     for entry in t1:
         filepath = unzipped + entry
-        fileType = subprocess.check_output(f"file {filepath}", shell=True).decode()
-    
-        folder = "other"
-        if "PE" in fileType : folder = "pe"
-        elif "ELF" in fileType : folder = "elf"
-        elif "MS-DOS" in fileType : folder = "msdos"
+        try: 
+            fileType = subprocess.check_output(f"file {filepath}", shell=True).decode()
+            folder = "other"
+            if "PE" in fileType : folder = "pe"
+            elif "ELF" in fileType : folder = "elf"
+            elif "MS-DOS" in fileType : folder = "msdos"
 
-        hashed = str(abs(hash(entry)))
-        imgpath = f"{dest}{folder}/{hashed}"
-        projetLib.data.extract_img(filepath,imgpath)
+            hashed = str(abs(hash(entry)))
+            imgpath = f"{dest}{folder}/{hashed}"
+            projetLib.data.extract_img(filepath,imgpath)
+        except Exception as e:print(e)
     shutil.rmtree(unzipped)
 
 def downloadAll(id,istart=0):
-    base = "./projetLib/data/images"
+    base = "./data/images"
     for i in reversed(range(id+3*istart,460,3)):
         i = str(i)
         i = "0"*(5-len(i)) + i
         dest = f"{base}/Virusshare{i}/"
-        if os.path.exists(dest):
+        if not os.path.exists(dest):
             os.makedirs(dest)
+        print(dest)
         url = f"https://samples.vx-underground.org/samples/Blocks/Virusshare%20Collection/Virusshare.{i}.7z"
         print("downloading ",url)
         download_data(url,dest)
