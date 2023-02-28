3c6d4aa160c068777b730d1eae58fbcb17a6c2c1
HeineKayn <61800617+HeineKayn@users.noreply.github.com>
2023-02-28 14:20:19 +0100

    push

--- projetLib/image.py
+++ projetLib/image.py
@@ -30,12 +30,12 @@ def getImageLoader(file:str,resize,doRGB,doCrop):
     ])
     return ImageFolder(file, process)
 
-def get_malware_dataset(resize,whitelist,doRGB):
+def get_malware_dataset(resize,whitelist,doRGB,doCrop):
     datasets = []
     for folder in os.listdir(imgpath):
         if folder != benign :
             newpath   = imgpath + folder + "/"
-            dataset   = getImageLoader(newpath,resize,doRGB)
+            dataset   = getImageLoader(newpath,resize,doRGB,doCrop)
             idwhitelist = [dataset.class_to_idx[x] for x in whitelist if x in dataset.class_to_idx.keys()]
             idx = [i for i in range(len(dataset)) if dataset.imgs[i][1] in idwhitelist]
             for i in range(len(dataset)):
@@ -44,12 +44,12 @@ def get_malware_dataset(resize,whitelist,doRGB):
             datasets.append(dataset)
     return torch.utils.data.ConcatDataset(datasets)
 
-def get_benign_dataset(resize, whitelist, doRGB):
+def get_benign_dataset(resize, whitelist, doRGB, doCrop):
     datasets = []
     path   = imgpath + benign + "/"
     for folder in os.listdir(path):
         newpath = path + folder + "/"
-        dataset   = getImageLoader(newpath,resize,doRGB)
+        dataset   = getImageLoader(newpath,resize,doRGB,doCrop)
         idwhitelist = [dataset.class_to_idx[x] for x in whitelist if x in dataset.class_to_idx.keys()]
         idx = [i for i in range(len(dataset)) if dataset.imgs[i][1] in idwhitelist]
         for i in range(len(dataset)):
@@ -58,20 +58,20 @@ def get_benign_dataset(resize, whitelist, doRGB):
         datasets.append(dataset)
     return torch.utils.data.ConcatDataset(datasets)
 
-def getTrainTest(resize=(224,224),batch_size=32,seed=1,trainSize=[],testSize=[],extensions=["pe","msdos","elf","other"],doRGB=False):
+def getTrainTest(resize=(224,224),batch_size=32,seed=1,trainSize=[],testSize=[],extensions=["pe","msdos","elf","other"],doRGB=False,doCrop=True):
     g = torch.Generator()
     if seed != 0 : g.manual_seed(seed)
 
     malTrain,benTrain = [],[]
     malTest,benTest   = [],[]
     for ext in extensions:
-        malExt = get_malware_dataset(resize,[ext],doRGB) 
+        malExt = get_malware_dataset(resize,[ext],doRGB,doCrop) 
         malExt,restMal = torch.utils.data.random_split(malExt, [trainSize[0]//len(extensions),len(malExt)-trainSize[0]//len(extensions)],g)
         malTrain.append(malExt)
         restMal,_ = torch.utils.data.random_split(restMal, [testSize[0]//len(extensions),len(restMal)-testSize[0]//len(extensions)],g)
         malTest.append(restMal)
 
-        benExt = get_benign_dataset(resize,[ext],doRGB)
+        benExt = get_benign_dataset(resize,[ext],doRGB,doCrop)
         benExt,restBen = torch.utils.data.random_split(benExt, [trainSize[1]//len(extensions),len(benExt)-trainSize[1]//len(extensions)],g)
         benTrain.append(benExt)
         restBen,_ = torch.utils.data.random_split(restBen, [testSize[1]//len(extensions),len(restBen)-testSize[1]//len(extensions)],g)
--- trainmodel.py
+++ trainmodel.py
@@ -5,9 +5,9 @@ import sys
 from torch.nn import BCEWithLogitsLoss, HingeEmbeddingLoss
 from statistics import mean
 
-runName = "400_crop"
+runName = "224_resize"
 batch_size = 16
-resize = (400,400)
+resize = (224,224)
 sizeTrain = [5000,5000]
 sizeTest = [1000,1000]
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
@@ -18,7 +18,7 @@ if len(sys.argv)>1:
 
 trainDataset, testDataset = proj.image.getTrainTest(
     resize=resize, batch_size=batch_size, trainSize=sizeTrain, testSize=sizeTest,
-    extensions=["pe"], seed=1, doRGB=False)
+    extensions=["pe"], seed=1, doRGB=False, doCrop=False)
 
 print(f"{runName} : Images de train {len(trainDataset)}({int(sizeTrain[0]/len(trainDataset)*100)}% malware), Images de test {len(testDataset)} ({int(sizeTest[0]/len(testDataset)*100)}% malware)")
 
